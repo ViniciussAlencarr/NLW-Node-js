07cb86e44a45db61c7460a14f8d29d59c318a149
ViniciussAlencarr <vinicius.alencaarr@gmail.com>
2021-06-25 22:13:47 -0300

    criando banco de dados sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata"
 import express from 'express'
+import './database'
 
 const app = express()
 
-app.get('/teste', (req, res, next) => {
-    return res.send('Hello World!')
-})
-
-app.post('/teste01', (req, res, next) => {
-    res.json({ message: 'Inserido com sucesso!'})
-})
 
 app.listen(3000, () => console.log('Server is running...'))
